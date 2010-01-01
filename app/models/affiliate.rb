211fc16fb6abc92c5fb9fc4964ceacd77fe3b262
David Clark <dmclark@gmail.com>
2010-01-01 14:38:10 -0500

    attempt to add ip  lookup to model

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,2 +1,10 @@
 class Affiliate < ActiveRecord::Base
+    
+  def get_ip
+      begin
+    ip = Resolv::getaddress(url)  
+  rescue Exception => e
+    ip = "<span class=\"error\">IP Check Error</span>"
+  end
+  end
 end
