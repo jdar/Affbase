b14fb00e99cf83d3ee57515720be82008233b3e2
David Clark <dmclark@gmail.com>
2010-01-04 14:59:31 -0500

    first iteration of webthumbs integration

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,11 +1,25 @@
 class Affiliate < ActiveRecord::Base
+  require 'resolv'
+  require 'rubygems'
+require 'rwebthumb'
+include Simplificator::Webthumb
 
-require 'resolv'
+  
+  
     
   def ip
     ip = Resolv::getaddress(url)  
     rescue Exception => e
     ip = "<span class=\"error\">IP Check Error</span>"
+  end
+  
+  # Modified from dzone webthumb shippet. This may not be right place. Helper? controller?
+  def img
+    # wt = Webthumb.new('a9fdb3aea9235c570cd22ecdc0a2c956')
+    # job = wt.thumbnail(:url => url)
+    # job.write_file(job.fetch_when_complete, "public/data/#{id}.png")
+    img = "/data/#{id}.png"
+
   end
 
 end
