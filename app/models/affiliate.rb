4dae60f404dbfde8bebc1ce73b9a04d5699804b3
David Clark <dmclark@gmail.com>
2010-01-05 14:31:48 -0500

    some refactoring

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -3,16 +3,8 @@ class Affiliate < ActiveRecord::Base
   require 'rubygems'
 require 'rwebthumb'
 include Simplificator::Webthumb
-
-  
-  
     
-  def ip
-    ip = Resolv::getaddress(url)  
-    rescue Exception => e
-    ip = "<span class=\"error\">IP Check Error</span>"
-  end
-  
+
   # Modified from dzone webthumb shippet. This may not be right place. Helper? controller?
   def img
     # wt = Webthumb.new('a9fdb3aea9235c570cd22ecdc0a2c956')
