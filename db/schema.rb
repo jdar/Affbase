8c6ad2de63b6536c0795eefd9e7f4d856eed1239
David Clark <dmclark@gmail.com>
2010-01-06 12:51:24 -0500

    view refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091231145514) do
+ActiveRecord::Schema.define(:version => 20100103214726) do
 
   create_table "affiliates", :force => true do |t|
     t.string   "org_name"
