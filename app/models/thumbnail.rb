aa7164b175082bee8fdfd28eb3dc92978f60c810
David Clark <dmclark@gmail.com>
2010-01-06 16:18:18 -0500

    Thumbnail model

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,5 +1,12 @@
 class Thumbnail
+  require "find"
+  
+  def initialize
+    @Thumbnails = Dir.glob("#{RAILS_ROOT}/PUBLIC/DATA/*.png")
+
+  end
+  
   def self.all
-    
+    Dir.glob("#{RAILS_ROOT}/PUBLIC/DATA/*.png")
   end
 end
