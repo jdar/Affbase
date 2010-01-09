42cb7c1b3428ae3fb7fc6d587562c8cf81fe5414
Greg Elin <greg@fotonotes.net>
2010-01-09 18:06:11 -0500

    Add Affiliate methods generate_thumbnail thumbnail_url thumbnail_localfile

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,17 +1,31 @@
 class Affiliate < ActiveRecord::Base
   require 'resolv'
   require 'rubygems'
-require 'rwebthumb'
-include Simplificator::Webthumb
-    
-
-  # Modified from dzone webthumb shippet. This may not be right place. Helper? controller?
-  def img
-    # wt = Webthumb.new('a9fdb3aea9235c570cd22ecdc0a2c956')
-    # job = wt.thumbnail(:url => url)
-    # job.write_file(job.fetch_when_complete, "public/data/#{id}.png")
-    img = "/data/#{id}.png"
-
+  require 'rwebthumb'
+  include Simplificator::Webthumb
+  
+  def inspect
+    "organization affiliate: #{org_name}"
   end
+  
+  def to_s
+    "#{org_name} (#{url})"
+  end
+
+  def generate_thumbnail
+    puts "# generate thumbnail"
+    wt = Webthumb.new('a9fdb3aea9235c570cd22ecdc0a2c956')
+    job = wt.thumbnail(:url => url)
+    job.write_file(job.fetch_when_complete, "public/data/#{id}.png")
+  end
+  
+  def thumbnail_url
+    "/public/data/#{id}.png"
+  end
+
+  def thumbnail_localfile
+    "#{RAILS_ROOT}/public/data/#{id}.png"
+  end
+  
 
 end
