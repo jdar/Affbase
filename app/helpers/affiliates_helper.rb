07a7e8beb509fca88c7166e63539d8cba0bc126d
David Clark <dmclark@gmail.com>
2010-01-11 21:03:19 -0500

    Added helper for hyperlinking external urls

--- app/helpers/affiliates_helper.rb
+++ app/helpers/affiliates_helper.rb
@@ -4,5 +4,9 @@ module AffiliatesHelper
     rescue Exception => e
     ip = "<span class=\"error\">IP Check Error</span>"
   end
+  def autolink string
+    "<a href=\"http://#{string}\">#{string}</a>"
+  end
+  
 
 end
