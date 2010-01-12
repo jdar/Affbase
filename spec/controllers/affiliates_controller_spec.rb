8af4557f5038d3709f692aaf9031b68846e4f907
David Clark <dmclark@gmail.com>
2010-01-12 16:52:56 -0500

    more work on tests

--- spec/controllers/affiliates_controller_spec.rb
+++ spec/controllers/affiliates_controller_spec.rb
@@ -2,15 +2,11 @@ require 'spec_helper'
 
 describe AffiliatesController do
 
-  def mock_affiliate(stubs={})
-    @mock_affiliate ||= mock_model(Affiliate, stubs)
-  end
 
   describe "GET index" do
     it "assigns all affiliates as @affiliates" do
-      Affiliate.stub!(:find).with(:all).and_return([mock_affiliate])
-      get :index
-      assigns[:affiliates].should == [mock_affiliate]
+      get 'index'
+      response.should be_success
     end
   end
   
@@ -18,24 +14,36 @@ describe AffiliatesController do
     it "Should list all affiliates in tabular form" do
       get 'list'
       response.should be_success
-          end
-  end
-
-  describe "GET show" do
-    it "assigns the requested affiliate as @affiliate" do
-      Affiliate.stub!(:find).with("37").and_return(mock_affiliate)
-      get :show, :id => "37"
-      assigns[:affiliate].should equal(mock_affiliate)
     end
   end
-
-  # describe "GET new" do
-  #   it "assigns a new affiliate as @affiliate" do
-  #     Affiliate.stub!(:new).and_return(mock_affiliate)
-  #     get :new
+  
+  describe "GET new" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+    
+    it "should show empty edit form" do
+      get 'new'
+      response.should render_template('affiliates/new')
+      
+    end
+  end
+  
+  # Autogenererated tests that I do not understand yet
+  
+  
+  # def mock_affiliate(stubs={})
+  #   @mock_affiliate ||= mock_model(Affiliate, stubs)
+  # end
+  # describe "GET show" do
+  #   it "assigns the requested affiliate as @affiliate" do
+  #     Affiliate.stub!(:find).with("37").and_return(mock_affiliate)
+  #     get :show, :id => "37"
   #     assigns[:affiliate].should equal(mock_affiliate)
   #   end
   # end
+
   # 
   # describe "GET edit" do
   #   it "assigns the requested affiliate as @affiliate" do
