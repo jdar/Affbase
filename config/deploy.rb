6949bb0193f56b9d027ffa2d1a4a1ecdd512e19a
David Clark <dmclark@gmail.com>
2010-01-14 13:53:02 -0500

    start of capification

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :user, 'lifewithoutlimitslabs.org'
+set :server, 'lifewithoutlimitslabs.org'
+set :application, "Affweb"
+set :repository,  "git://github.com/LifeWithoutLimits/Affbase.git"
+set :keep_releases, 5
+role :web, server
+role :app, server
+role :db,  server, :primary => true
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/home/#{user}/#{application}" 
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+task :restart, :roles => :app do
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+after "deploy:update_code", :roles => [:web, :db, :app] do
+  run "chmod 755 #{release_path}/public -R" 
+end
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy:update", "deploy:cleanup" 
