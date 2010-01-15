dcb07d9192555307ee3f3efbceae246830b09579
David Clark <dmclark@gmail.com>
2010-01-15 14:14:59 -0500

    capistrano work

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,30 @@ set :user, 'dclark'
 set :server, 'lifewithoutlimitslabs.org'
 set :application, 'Affbase'
 set :repository,  'git@github.com:LifeWithoutLimits/Affbase.git'
+set :branch, "master"
 set :keep_releases, 5
 server 'www.lifewithoutlimitslabs.org', :app, :web, :db
-# 
-# role :web, "www.lifewithoutlimitslabs.org"
-# role :app, "www.lifewithoutlimitslabs.org"
-# role :db,  "www.lifewithoutlimitslabs.org", :primary => true
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :scm, "git"
+set :git_enable_submodules, 1
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-# set :deploy_to, "/var/#{application}"
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/sensitive.yml #{release_path}/config/sensitive.yml"
+  end
+  
+  desc "Sync the public/assets directory."
+  task :assets do
+    system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared
