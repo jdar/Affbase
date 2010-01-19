1f7f50983cde3ce3fb85c43a10be00a541d3d4f3
David Clark <dmclark@gmail.com>
2010-01-18 21:02:55 -0500

    Use domain for urls & filenames

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -4,28 +4,31 @@ class Affiliate < ActiveRecord::Base
   require 'rwebthumb'
   include Simplificator::Webthumb
   validates_uniqueness_of :domain
-  validates_presence_of [:org_name, :domain, :url], :message => "can't be blank"
+  validates_presence_of [:org_name, :url], :message => "can't be blank"
   
   after_create :generate_thumbnail 
   
-  def inspect
-    "organization affiliate: #{org_name}"
+  def to_param
+    "#{id}-#{url_id}"
   end
   
+  def url_id
+    domain.chomp.gsub(".","")
+  end
   
   
   def generate_thumbnail
     wt = Webthumb.new(APP_CONFIG['webthumb_api']['key'])
     job = wt.thumbnail(:url => url)
-    job.write_file(job.fetch_when_complete(:medium2), "#{RAILS_ROOT}/public/data/#{id}.png")
+    job.write_file(job.fetch_when_complete(:medium2), "#{RAILS_ROOT}/public/data/#{url_id}.png")
   end
   
   def thumbnail_url
-    "/data/#{id}.png"
+    "/data/#{url_id}.png"
   end
 
   def thumbnail_localfile
-    File.exist?("#{RAILS_ROOT}/public/data/#{id}.png") ? "#{RAILS_ROOT}/public/data/#{id}.png" : nil
+    File.exist?("#{RAILS_ROOT}/public/data/#{url_id}.png") ? "#{RAILS_ROOT}/public/data/#{url_id}.png" : nil
   end
   
   def thumbnail_last_updated
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
-   map.connect 'affiliates/list', :controller => 'affiliates', :action => 'list'
+  map.connect 'affiliates/list', :controller => 'affiliates', :action => 'list'
   map.resources :affiliates
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
