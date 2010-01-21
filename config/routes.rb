9b685465c4ee7533ac62160eef4517035c515108
David Clark <dmclark@gmail.com>
2010-01-21 18:54:07 -0500

    Updates for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect 'affiliates/list', :controller => "affiliates", :action => "list"  
+  map.resources :affiliates
 
   map.connect ":controller/:action"
-  map.resources :affiliates
   map.connect '', :controller => "affiliates" 
   map.connect ":controller/:action/:id"
   map.connect ":controller/:action/:id/:format"
