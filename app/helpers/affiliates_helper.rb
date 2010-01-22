ac118aa812a147fe248f1999892fcaba1722c112
David Clark <dmclark@gmail.com>
2010-01-22 11:14:16 -0500

    Dont lookup ip in dev env

--- app/helpers/affiliates_helper.rb
+++ app/helpers/affiliates_helper.rb
@@ -1,9 +1,19 @@
 module AffiliatesHelper
+  
+  # ip lookup can be resource intensive - skip on dev
   def get_ip url
+    unless RAILS_ENV == 'development'
+      ip url    
+    end
+  end
+  
+  def ip(url)
     ip = Resolv::getaddress(url)  
     rescue Exception => e
     ip = "<span class=\"error\">IP Check Error</span>"
   end
+  
+  
   def autolink string
     "<a href=\"http://#{string}\">#{string}</a>"
   end
