dda39c67b8b2cc609abb463f01fd0e7711db5b12
David Clark <dmclark@gmail.com>
2010-01-21 20:12:39 -0500

    custom route for regenerate_thumb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'affiliates/list', :controller => "affiliates", :action => "list"  
+  map.connect 'affiliates/regenerate_thumb/:id', :controller => "affiliates", :action => "regenerate_thumb"
   map.resources :affiliates
 
   map.connect ":controller/:action"
