8c257469eac408a75964c75e5e46ef23052c7527
David Clark <dmclark@gmail.com>
2010-02-02 20:32:54 -0500

    Move update feed to controller & geocode

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -28,40 +28,7 @@ class AffiliatesController < ApplicationController
  # GET /affiliates/getfeed
   def feed
     @affiliates = Affiliate.all
-    require 'yaml'
-    require 'open-uri'
-    @feed = YAML.load(open("http://ucp.org/sandbox/activeaffiliates.cfm?apikey=#{APP_CONFIG['ucp_api']['key']}"))
-    @feed.each do |f,org|
-      a = Affiliate.find_by_org_name(org["affiliate_name"].chomp(';')) 
-        unless a.nil?
-          a.siteid = org["affiliate_id"].chomp(';') 
-          a.name_abbr = org["affiliate_name_abbr"].chomp(';')
-          a.address1 = org["affiliate_address1"].value.chomp(';')
-          a.city = org["affiliate_city"].chomp(';')
-          a.state = org["affiliate_state"].chomp(';')  
-          a.zip = org["affiliate_zip"].chomp(';')
-          a.contact_first_name = org["affiliate_contact_first_name"].chomp(';').delete '~'
-          a.contact_last_name = org["affiliate_contact_last_name"].chomp(';').delete '~'
-          a.contact_email = org["affiliate_contact_email"].chomp(';').delete '~'
-          a.phone1 = org["affiliate_phone1"].chomp(';').delete '~'
-          a.phone2 = org["affiliate_phone2"].chomp(';').delete '~'
-          a.tty = org["affiliate_tty"].chomp(';').delete '~'
-          a.fax = org["affiliate_fax"].chomp(';').delete '~'
-          a.email = org["affiliate_email"].chomp(';').delete '~'
-          a.description = org["affiliate_description"].chomp(';').delete '~'
-          a.metric      = org["affiliate_metric"].chomp(';').delete '~'
-          a.activate    = org["affiliate_activate"].chomp(';').delete '~'
-          a.public      = org["affiliate_public"].chomp(';').delete '~'
-          a.region      = org["affiliate_region"].chomp(';').delete '~'
-          a.province    = org["affiliate_province"].chomp(';').delete '~'
-          a.country     = org["affiliate_country"].chomp(';').delete '~'
-          a.aff_type        = org["affiliate_type"].chomp(';').delete '~'
-          a.template    = org["affiliate_template"].chomp(';')
-          a.state_ind   = org["affiliate_state_ind"].chomp(';')
-          a.save
-        end
-    end
-      
+   
     
     respond_to do |format|
       # flash[:notice] = "#{@feed.count} records available "
--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -5,7 +5,7 @@ class Affiliate < ActiveRecord::Base
   include Simplificator::Webthumb
   validates_uniqueness_of :domain
   validates_presence_of [:org_name, :url], :message => "can't be blank"
-  validates_presence_of :lat, :long
+  # validates_presence_of :lat, :long
   acts_as_mappable :auto_geocode => true 
   has_many :events
   
@@ -47,6 +47,56 @@ class Affiliate < ActiveRecord::Base
   def contact_name
     contact_first_name + " " + contact_last_name
   end
+
   
+  def combined_addsess
+    address1 + ', ' + city + ', ' + state
+    
+  end
+  
+    
+  def geocode_address
+    geo = GeoKit::Geocoders::MultiGeocoder.geocode(combined_addsess)
+    errorrs.add(:combined_addsess, "Could no geocode #{combined_addsess}") unless geo.success
+    self.lat, self.long = geo.lat, geo.lng if geo.success
+  end
+  
+  
+  def self.update_from_affnet
+     require 'yaml'
+    require 'open-uri'
+    @feed = YAML.load(open("http://ucp.org/sandbox/activeaffiliates.cfm?apikey=#{APP_CONFIG['ucp_api']['key']}"))
+    @feed.each do |f,org|
+     a = Affiliate.find_by_org_name(org["affiliate_name"].chomp(';')) 
+        unless a.nil?
+          a.siteid = org["affiliate_id"].chomp(';') 
+          a.name_abbr = org["affiliate_name_abbr"].chomp(';')
+          a.address1 = org["affiliate_address1"].value.chomp(';')
+          a.city = org["affiliate_city"].chomp(';')
+          a.state = org["affiliate_state"].chomp(';')  
+          a.zip = org["affiliate_zip"].chomp(';')
+          a.contact_first_name = org["affiliate_contact_first_name"].chomp(';').delete '~'
+          a.contact_last_name = org["affiliate_contact_last_name"].chomp(';').delete '~'
+          a.contact_email = org["affiliate_contact_email"].chomp(';').delete '~'
+          a.phone1 = org["affiliate_phone1"].chomp(';').delete '~'
+          a.phone2 = org["affiliate_phone2"].chomp(';').delete '~'
+          a.tty = org["affiliate_tty"].chomp(';').delete '~'
+          a.fax = org["affiliate_fax"].chomp(';').delete '~'
+          a.email = org["affiliate_email"].chomp(';').delete '~'
+          a.description = org["affiliate_description"].chomp(';').delete '~'
+          a.metric      = org["affiliate_metric"].chomp(';').delete '~'
+          a.activate    = org["affiliate_activate"].chomp(';').delete '~'
+          a.public      = org["affiliate_public"].chomp(';').delete '~'
+          a.region      = org["affiliate_region"].chomp(';').delete '~'
+          a.province    = org["affiliate_province"].chomp(';').delete '~'
+          a.country     = org["affiliate_country"].chomp(';').delete '~'
+          a.aff_type        = org["affiliate_type"].chomp(';').delete '~'
+          a.template    = org["affiliate_template"].chomp(';')
+          a.state_ind   = org["affiliate_state_ind"].chomp(';')
+          a.geocode_address
+          a.save
+        end
+    end
+  end
 
 end
